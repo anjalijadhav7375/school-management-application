964dc704fd2a29ff2bc9fbda0f4b927d3e509d17
pravin4399 <pravintupe2213@gmail.com>
2023-03-29 18:55:56 +0530

    Complete School-Management Application

--- src/main/java/com/anjali/exception/AdmissionNotFoundException.java
+++ src/main/java/com/anjali/exception/AdmissionNotFoundException.java
@@ -2,7 +2,7 @@ package com.anjali.exception;
 
 public class AdmissionNotFoundException extends RuntimeException {
     public AdmissionNotFoundException(Long id){
-        super("Cou;d not find admission " + id);
+        super("Could not find admission " + id);
     }
 
 }
